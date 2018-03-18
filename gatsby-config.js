8bf4ee894157b18ecf8bd76c6a29d0c8e9aa533f
Rebekah Jones <rebekah.jones@inviqa.com>
2018-03-18 22:18:12 +0000

    add headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,29 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
+    },
+    {
+      resolve: "gatsby-plugin-netlify-headers",
+      options: {
+        headers: {
+          "/": ["Cache-Control: public, max-age=600"],
+          "/dracula.css": ["Cache-Control: public, max-age=31536000"],
+          "/fonts.css": ["Cache-Control: public, max-age=600"],
+          "/*": [
+            "Strict-Transport-Security: max-age=31536000; includeSubDomains"
+          ],
+          "/*.css": ["X-Content-Type-Options: nosniff"],
+          "/*.js": [
+            "Content-Type: text/javascript; charset=utf-8",
+            "Cache-Control: public, max-age=600",
+            "X-Content-Type-Options: nosniff"
+          ],
+          "/favicon.ico": [
+            "Content-Type: image/x-icon",
+            "Cache-Control: public, max-age=600"
+          ]
+        }
+      }
     }
   ]
 };
